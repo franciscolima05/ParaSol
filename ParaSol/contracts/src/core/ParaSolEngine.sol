373bf793289c0c56da85da3db7cdf5958d1f4101
Gonzalo <Gonzaromano35@gmail.com>
2026-05-15 23:21:29 -0300

    implements state variables, events, and a constructor

--- ParaSol/contracts/src/core/ParaSolEngine.sol
+++ ParaSol/contracts/src/core/ParaSolEngine.sol
@@ -14,3 +14,25 @@ interface IParaSolPolicy {
 interface IParaSolPool {
     function executePayout(uint256 poolId, address farmerAddress, uint256 payoutAmount) external;
 }
+
+contract ParaSolEngine is Ownable {
+    IParaSolPolicy public policyContract;
+    IParaSolPool public poolContract;
+    
+    address public oracle; 
+
+    mapping(uint256 => uint256) public percentagePaidOut;
+
+    event PayoutTriggered(uint256 indexed policyId, uint256 ndviDrop, uint256 payoutPercent, uint256 amountUSDC);
+
+    modifier onlyOracle() {
+        require(msg.sender == oracle, "Solo el oraculo (Backend) puede reportar");
+        _;
+    }
+
+    constructor(address _policyAddress, address _poolAddress, address _oracleAddress, address initialOwner) Ownable(initialOwner) {
+        policyContract = IParaSolPolicy(_policyAddress);
+        poolContract = IParaSolPool(_poolAddress);
+        oracle = _oracleAddress;
+    }
+}
