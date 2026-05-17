3bf0d80bb6ff2762d76332c56075a34ae67d88cc
yumanyer <manuelpisoni09@gmail.com>
2026-05-17 03:52:45 -0300

    front online

--- ParaSol/backend/app/main.py
+++ ParaSol/backend/app/main.py
@@ -4,39 +4,26 @@ from app.core.earth_engine import init_ee
 from app.api.routes.vegetation import router as vegetation_router
 from pathlib import Path
 from dotenv import load_dotenv
-
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-app = FastAPI()
 
-#REQUEST = datos del cliente
-#POLYGON = objeto GIS real
-#DATES = fechas
-#COLLECTION = colección de imágenes
-#FIRST IMAGE = primera imagen de la colección
-#BANDS = una capa de información dentro de una imagen
-#RAINFALL BANDS = bandas del dataset => Earth Engine: sumó las 365 imágenes (lluvia total acumulada del año)
-#STATS = estadísticas de la región
+BASE_DIR = Path(__file__).resolve().parents[2]  # ParaSol/
+
+load_dotenv(BASE_DIR / ".env.dev")
+
+SPICH_DIR = BASE_DIR / "frontend" / "web" / "spich"
+
+app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
     init_ee()
 
-
 app.include_router(rainfall_router)
 app.include_router(vegetation_router)
 
-
-BASE_DIR = Path(__file__).resolve().parents[2]  # ParaSol/
-env_path = BASE_DIR / ".env.dev"
-
-print(f"Buscando .env en: {env_path}")
-print(f"Existe: {env_path.exists()}")
-
-load_dotenv(env_path)
-
-app.mount("/static", StaticFiles(directory="../frontend/web/spich"), name="static")
+app.mount("/static", StaticFiles(directory=str(SPICH_DIR)), name="static")
 
 @app.get("/")
 def serve_index():
-    return FileResponse("../frontend/web/spich/index.html")
+    return FileResponse(str(SPICH_DIR / "index.html"))
